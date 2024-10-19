f3fb4954ac88af525f2af900150f64d3f6875230
maomao-samantha <165502904+maomao-samantha@users.noreply.github.com>
2024-10-19 12:08:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import random
 
 def obtener_recomendacion():
     acciones = [
-        "Evita conducir, usa la bicicleta o camina.",
-            "Usa el transporte público para reducir emisiones de CO₂,CO, NOx entre otros .",
-            "Planta un árbol o cuida plantas en tu hogar par aumentar la combercion de Dióxido de carbono (CO₂) a Oxígeno.",
-            "Reduce el uso de plásticos, lleva tu propia bolsa.",
-            "Desconecta los dispositivos electrónicos que no usas.",
-            "Participa u organiza actividades de limpieza comunitaria.",
-            "Ahorra agua mientras te duchas o lavas los platos.",
-            "Evita quemar basura, hojas y otros objetos, así como hacer fogatas en bosques o en plena ciudad.",
-            "Recicla y separa tus residuos correctamente para obtener mas informacion porfavor visualizar el siguiente video https://www.youtube.com/watch?v=IoWmF0fo2Wc.",
-            "Apoya empresas que utilizan prácticas sostenibles como Grupo Bimbo que ha realizado inversiones significativas en energía eólica y solar, coca cola con iniciativa global para el reciclaje de envases. Bajo el lema “Un Mundo sin Residuos”, Coca-Cola Además, Coca-Cola se ha enfocado en la gestión sostenible del agua, Banco Santander Se ha posicionado como una entidad financiera líder en sostenibilidad, integrando prácticas responsables y ecológicas en su modelo de negocio entre otras ."
+                "Evita conducir, usa la bicicleta o camina.",
+                "Usa el transporte público para reducir emisiones de CO₂,CO, NOx entre otros .",
+                "Planta un árbol o cuida plantas en tu hogar par aumentar la combercion de Dióxido de carbono (CO₂) a Oxígeno.",
+                "Reduce el uso de plásticos, lleva tu propia bolsa.",
+                "Desconecta los dispositivos electrónicos que no usas.",
+                "Participa u organiza actividades de limpieza comunitaria.",
+                "Ahorra agua mientras te duchas o lavas los platos.",
+                "Evita quemar basura, hojas y otros objetos, así como hacer fogatas en bosques o en plena ciudad.",
+                "Recicla y separa tus residuos correctamente para obtener mas informacion porfavor visualizar el siguiente video https://www.youtube.com/watch?v=IoWmF0fo2Wc.",
+                "Apoya empresas que utilizan prácticas sostenibles como Grupo Bimbo que ha realizado inversiones significativas en energía eólica y solar, coca cola con iniciativa global para el reciclaje de envases. Bajo el lema “Un Mundo sin Residuos”, Coca-Cola Además, Coca-Cola se ha enfocado en la gestión sostenible del agua, Banco Santander Se ha posicionado como una entidad financiera líder en sostenibilidad, integrando prácticas responsables y ecológicas en su modelo de negocio entre otras ."
     ]
     
     return random.choice(acciones)
